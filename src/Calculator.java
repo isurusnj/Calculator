dcbd7448b2efd27aece5ca4ce1539fa72dcff8f7
Isuru <nuwansnj99@gmail.com>
2025-02-22 23:40:54 +0530

    Calculator app therd commit - button has created

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,16 +5,60 @@ import java.awt.event.*;
 public class Calculator implements ActionListener {
 
     JFrame frame;
-    JTextField textField;
-    JButton [] numberButtons = new JButton[10];
-    JButton[] functionButtons = new JButton[10];
-    JButton[] addButtons,subbuttons,mulButtons,divButtons;
-    JButton decButton,eqeButton,delButton,clrButton;
+    JTextField textfield;
+    JButton[] numberButtons = new JButton[10];
+    JButton[] functionButtons = new JButton[9];
+    JButton addButton,subButton,mulButton,divButton;
+    JButton decButton, equButton, delButton, clrButton, negButton;
     JPanel panel;
 
-    Font font = new Font("Arial", Font.PLAIN, 12);
+    Font myFont = new Font("Ink Free",Font.BOLD,30);
+
     double num1=0,num2=0,result=0;
+    char operator
     Calculator(){
+        frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 550);
+        frame.setLayout(null);
+
+        textfield = new JTextField();
+        textfield.setBounds(50, 25, 300, 50);
+        textfield.setFont(myFont);
+        textfield.setEditable(false);
+
+        addButton = new JButton("+");
+        subButton = new JButton("-");
+        mulButton = new JButton("*");
+        divButton = new JButton("/");
+        decButton = new JButton(".");
+        equButton = new JButton("=");
+        delButton = new JButton("Del");
+        clrButton = new JButton("Clr");
+        negButton = new JButton("(-)");
+
+        functionButtons[0] = addButton;
+        functionButtons[1] = subButton;
+        functionButtons[2] = mulButton;
+        functionButtons[3] = divButton;
+        functionButtons[4] = decButton;
+        functionButtons[5] = equButton;
+        functionButtons[6] = delButton;
+        functionButtons[7] = clrButton;
+        functionButtons[8] = negButton;
+
+        for(int i =0;i<9;i++) {
+            functionButtons[i].addActionListener(this);
+            functionButtons[i].setFont(myFont);
+            functionButtons[i].setFocusable(false);
+        }
+
+        for(int i =0;i<10;i++) {
+            numberButtons[i] = new JButton(String.valueOf(i));
+            numberButtons[i].addActionListener(this);
+            numberButtons[i].setFont(myFont);
+            numberButtons[i].setFocusable(false);
+        }
     }
 
     public static void main(String[] args) {
